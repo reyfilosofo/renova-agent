5ecb1e76c20ac57a179bf317bed7f74a5064057b
reyfilosofo <c.jonathan.gr@gmail.com>
2026-06-29 10:44:42 -0600

    Add Renova core package

---
+++ renova_core/__init__.py
@@ -0,0 +1,6 @@
+"""Renova Core: open-source computational scaffolds for the Renova project."""
+
+from .index import DimensionScore, RenovaAssessment, calculate_irg
+
+__all__ = ["DimensionScore", "RenovaAssessment", "calculate_irg"]
+__version__ = "0.1.0"
